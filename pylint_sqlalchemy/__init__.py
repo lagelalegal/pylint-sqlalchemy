8d55b71d5d2776ae709cddf9b6dd9290dfa961c2
lagel <lagel@live.com>
2016-08-22 15:46:04 +0800

    Add hack for sqlalchemy session member problem

---
+++ pylint_sqlalchemy/__init__.py
@@ -0,0 +1,6 @@
+"""Pylint sqlalchemy module."""
+# pylint: skip-file
+
+from pylint_sqlalchemy import plugin
+
+register = plugin.register
---
+++ pylint_sqlalchemy/plugin.py
@@ -0,0 +1,30 @@
+"""Pylint sqlalchemy plugin module."""
+import astroid
+
+from astroid import MANAGER
+from astroid.builder import AstroidBuilder
+
+from pylint_sqlalchemy.orm import Session
+
+
+def register(linter):
+    """Registering additional checkers."""
+    pass
+
+
+def handle_session(cls):
+    """Handle the sqlalchemy.orm.scoping.scoped_session."""
+    if cls.name == 'scoped_session':
+        source_files = cls.parent.source_file.split('/')
+        source_files[-1] = 'session.py'
+        module = AstroidBuilder(MANAGER).file_build(
+            '/'.join(source_files), 'sqlalchemy.orm.session'
+        )
+        session = module.locals.get('Session')[0]
+        for method in Session.public_methods:
+            cls.locals[method] = session.locals[method]
+
+        return cls
+
+
+MANAGER.register_transform(astroid.Class, handle_session)
